f4fa970c5736571352d3367904975c1e6d06a2db
chauncey <chaunceyjiang@gmail.com>
2020-03-22 01:01:02 +0800

    增加对象到数据库的转化

---
+++ schema/schema.go
@@ -0,0 +1,65 @@
+// ORM核心 对象(object)与数据库表(table)的转化
+package schema
+
+import (
+	"go/ast"
+	"orm/dialect"
+	"reflect"
+)
+
+// 数据库中有 表名 列名 列类型 约束
+// 对象有    结构体    字段   TAG
+
+// Field 字段<---> 列名
+type Field struct {
+	Name string // 对象的字段名
+	Type string // 对象字段的字段类型
+	Tag  string // 对象字段的Tag
+}
+
+// Schema 代表数据库中的一张表
+type Schema struct {
+	Model      interface{}       // 被映射对象Model
+	Name       string            // 表名
+	Fields     []*Field          // 被映射对象的所有字段
+	FieldNames []string          // 被映射对象的所有字段名字
+	fieldMap   map[string]*Field // 记录字段名与Field的关系，方便以后使用
+}
+
+// GetField 根据字段名字直接获取 Field
+func (s *Schema) GetField(name string) *Field {
+	return s.fieldMap[name]
+}
+
+// Parse 将任意对象Model解析为数据库中的一张表
+// 利用反射
+func Parse(dest interface{}, d dialect.Dialect) *Schema {
+	modelType := reflect.Indirect(reflect.ValueOf(dest)).Type()
+
+	schema := &Schema{
+		Model:    dest,
+		Name:     modelType.Name(),
+		fieldMap: make(map[string]*Field),
+	}
+	// 依次获取该对象的每一个字段
+	for i := 0; i < modelType.NumField(); i++ {
+		f := modelType.Field(i)
+
+		if !f.Anonymous && ast.IsExported(f.Name) {
+			// 不是匿名字段且可导出
+			field := &Field{
+				Name: f.Name,
+				Type: d.DataTypeOf(reflect.Indirect(reflect.New(f.Type))),
+			}
+			// 获取tag 中orm 标示
+			if v, ok := f.Tag.Lookup("orm"); ok {
+				field.Tag = v
+			}
+
+			schema.FieldNames = append(schema.FieldNames, f.Name)
+			schema.Fields = append(schema.Fields, field)
+			schema.fieldMap[f.Name] = field
+		}
+	}
+	return schema
+}
---
+++ schema/schema_test.go
@@ -0,0 +1,30 @@
+package schema
+
+import (
+	"orm/dialect"
+	"orm/ormlog"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	// 表名 User
+	type User struct {
+		// 主键约束
+		Name string `orm:"PRIMARY KEY"`
+		Age  int
+	}
+	d, _ := dialect.GetDialect("sqlite3")
+	s := Parse(&User{"foo", 18}, d)
+	if s.Name != "User" {
+		ormlog.Error("表名映射错误")
+		t.Fail()
+	}
+	if s.Fields[1].Name != "Age" {
+		ormlog.Error("列名映射错误")
+		t.Fail()
+	}
+	if s.GetField("Name").Tag != "PRIMARY KEY" {
+		ormlog.Error("约束解析错误")
+		t.Fail()
+	}
+}
