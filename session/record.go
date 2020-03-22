49e769d1ccdda75e30c83b299a3697495df765be
chauncey <chaunceyjiang@gmail.com>
2020-03-22 18:18:26 +0800

    实现FIND语句

--- session/record.go
+++ session/record.go
@@ -1,6 +1,10 @@
 package session
 
-import "orm/clause"
+import (
+	"errors"
+	"orm/clause"
+	"reflect"
+)
 
 // Insert 将对象的值转换为SQL，然后将数据插入数据库表中
 // u1 := &User{Name: "Sam", Age: 25} s.Insert(u1, u2, ...)
@@ -26,4 +30,47 @@ func (s *Session) Insert(objs ...interface{}) (int64, error) {
 		return 0, err
 	}
 	return result.RowsAffected()
+}
+
+// Find 根据传入的对象，查找相应的表，然后将数据依次填充到对应字段
+// Find([]User)
+func (s *Session) Find(objs interface{}) error {
+	if reflect.Indirect(reflect.ValueOf(objs)).Kind() != reflect.Slice {
+		return errors.New("Unsupported! expect Slice ")
+	}
+	objSlice := reflect.Indirect(reflect.ValueOf(objs))
+	// Elem 获取Slice 中的元素
+	objType := objSlice.Type().Elem()
+
+	// 根据objs列表中反射出列表中的类型,然后创建相应的schema
+	table := s.Model(reflect.New(objType).Elem().Interface()).RefTable()
+
+	// _select(tableName string, fields []string)
+	s.clause.Set(clause.SELECT, table.Name, table.FieldNames)
+
+	sql, sqlVars := s.clause.BuildSQL(clause.SELECT)
+
+	raws, err := s.Raw(sql, sqlVars...).QueryRaws()
+	if err != nil {
+		return err
+	}
+
+	for raws.Next() {
+		// 创建一个全部是空值的新obj
+		obj := reflect.New(objType).Elem()
+		var values []interface{}
+		for _, name := range table.FieldNames {
+			// 获取新obj的每个字段的地址
+			values = append(values, obj.FieldByName(name).Addr().Interface())
+		}
+
+		// 查询数据库，同时通过地址给每个字段赋值
+		if err := raws.Scan(values...); err != nil {
+			return err
+		}
+		// 将新的Obj 重新追加到 传递的Slice中
+		objSlice.Set(reflect.Append(objSlice, obj))
+	}
+
+	return raws.Close()
 }
--- session/record_test.go
+++ session/record_test.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
 	"orm/dialect"
+	"orm/ormlog"
 	"testing"
 )
 
@@ -22,7 +23,6 @@ func TestSession_Insert(t *testing.T) {
 	sess := New(db, d)
 
 	sess = sess.Model(&UserTest{})
-
 
 	if !sess.HasTable() {
 		sess.CreateTable()
@@ -43,4 +43,51 @@ func TestSession_Insert(t *testing.T) {
 	}
 
 	sess.DropTable()
+}
+
+func TestSession_Find(t *testing.T) {
+	type UserTest struct {
+		// 主键约束
+		Name string `orm:"PRIMARY KEY"`
+		Age  int
+	}
+	db, err := sql.Open("sqlite3", "../orm.db")
+	if err != nil {
+		t.FailNow()
+	}
+	defer db.Close()
+	d, _ := dialect.GetDialect("sqlite3")
+	sess := New(db, d)
+
+	sess = sess.Model(&UserTest{})
+
+	if !sess.HasTable() {
+		sess.CreateTable()
+	}
+	defer sess.DropTable()
+	sess.Insert(&UserTest{
+		Name: "foo",
+		Age:  18,
+	}, &UserTest{
+		Name: "bar",
+		Age:  25,
+	})
+
+	var users []UserTest
+	err = sess.Find(&users)
+
+	if err != nil {
+		ormlog.Error(err)
+		t.FailNow()
+	}
+	if users[0].Name != "foo" {
+		ormlog.Error("find name error")
+		t.FailNow()
+	}
+
+	if users[1].Age != 25 {
+		ormlog.Error("find age error")
+		t.FailNow()
+	}
+
 }
